841d48de1efa05481fe21f4f058b573f08d4c554
PortfolioJankowski <mat.jankowski99@gmail.com>
2025-01-05 10:59:40 +0100

    Add JsonFileReader functionallity

---
+++ src/Task/Services/FileReader/JsonFileReader.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using Task.Interfaces;
+using Task.Models;
+using Task.Services.Static;
+
+namespace Task.Services.FileReader
+{
+    public class JsonFileReader : IWebFileReader
+    {
+        public async Task<IEnumerable<Participant>> ReadParticipantsFromWebFileAsync(string url)
+        {
+            return await FileDownloader.DownloadJsonDataAsync(url);
+        }
+    }
+}
